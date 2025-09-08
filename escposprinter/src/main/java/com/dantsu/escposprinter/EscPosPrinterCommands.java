7b60d378c6c2e5b7d7f27607825727aa5d025e4e
taylorlacort <taylorlacortsuporte@gmail.com>
2025-09-08 16:56:18 -0300

    chore: bump version to 5.0.0

--- escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinterCommands.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinterCommands.java
@@ -57,7 +57,8 @@ public class EscPosPrinterCommands {
     public static final byte[] TEXT_DOUBLE_STRIKE_OFF = new byte[]{0x1B, 0x47, 0x00};
     public static final byte[] TEXT_DOUBLE_STRIKE_ON = new byte[]{0x1B, 0x47, 0x01};
 
-
+    public static final byte[] TEXT_COLOR_BLACK = new byte[]{0x1B, 0x72, 0x00};
+    public static final byte[] TEXT_COLOR_RED = new byte[]{0x1B, 0x72, 0x01};
 
     public static final byte[] TEXT_COLOR_REVERSE_OFF = new byte[]{0x1D, 0x42, 0x00};
     public static final byte[] TEXT_COLOR_REVERSE_ON = new byte[]{0x1D, 0x42, 0x01};
@@ -447,6 +448,9 @@ public class EscPosPrinterCommands {
         if (textSize == null) {
             textSize = EscPosPrinterCommands.TEXT_SIZE_NORMAL;
         }
+        if (textColor == null) {
+            textColor = EscPosPrinterCommands.TEXT_COLOR_BLACK;
+        }
         if (textReverseColor == null) {
             textReverseColor = EscPosPrinterCommands.TEXT_COLOR_REVERSE_OFF;
         }
@@ -528,6 +532,7 @@ public class EscPosPrinterCommands {
         try {
             this.printerConnection.write(new byte[]{0x1B, 0x74, (byte) charsetId});
             this.printerConnection.write(EscPosPrinterCommands.TEXT_SIZE_NORMAL);
+            this.printerConnection.write(EscPosPrinterCommands.TEXT_COLOR_BLACK);
             this.printerConnection.write(EscPosPrinterCommands.TEXT_COLOR_REVERSE_OFF);
             this.printerConnection.write(EscPosPrinterCommands.TEXT_WEIGHT_NORMAL);
             this.printerConnection.write(EscPosPrinterCommands.TEXT_UNDERLINE_OFF);
